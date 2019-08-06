c1881986902ceb5c594740813b90855c8ac41d2a
lkrjunior <lkrjunior@terra.com.br>
2019-08-06 20:57:53 -0300

    Beauty Scheduling

---
+++ src/main/java/com/lucianorocha/beautyscheduling/BeautyschedulingApplication.java
@@ -0,0 +1,13 @@
+package com.lucianorocha.beautyscheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautyschedulingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautyschedulingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucianorocha/beautyscheduling/BeautyschedulingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lucianorocha.beautyscheduling;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BeautyschedulingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
